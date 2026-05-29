9ecf5467500174c0fbe48984aae7954d45753503
koalablade <123zacky.h@gmail.com>
2026-05-29 16:06:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -223,3 +223,8 @@ def sync():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
